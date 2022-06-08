7ccefd857adf31dd29181c88d7f304f7d621b2c6
Ritika <ritikaritz8377@gmail.com>
2022-06-08 21:22:30 +0530

    Add all the files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-
+// npm i react-router-dom@5.3.1
+import React, { useContext } from 'react';
+import Feed from "./components/Feed";
+import Login from "./components/Login";
+import PageNotFound from "./components/PageNotFound";
+import Profile from "./components/Profile";
+import Signup from "./components/Signup";
+import { AuthContextProvider, AuthContext } from "./context/AuthContext";
+import { Switch, Route, Redirect } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* which component should render on which path */}
+      <AuthContextProvider>
+        <Switch>
+          <RedirectToFeed path="/login" comp={Login}>
+          </RedirectToFeed>
+          <RedirectToFeed path="/signup" comp={Signup}>
+          </RedirectToFeed>
+          <PrivateRoute path="/feed"
+            comp={Feed}
+          >
+          </PrivateRoute>
+          <PrivateRoute path="/profile"
+            comp={Profile}
+          >
+          </PrivateRoute>
+          <Redirect from="/" to="/feed"></Redirect>
+          <Route>
+            <PageNotFound></PageNotFound>
+          </Route>
+        </Switch>
+      </AuthContextProvider>
+    </>
   );
 }
+function PrivateRoute(props) {
+  console.log("private Route", props)
+  let Component = props.comp;
+  // check -> are you loggedIN
+  let cUser = useContext(AuthContext);
+  // cuser-> null ->logi page
+  // cuser-> anything 
+  return (
+    <Route
+      {...props}
+      render={
+        (props) => {
+          // logic
+          return cUser != null ? <Component {...props}>
+          </Component> : <Redirect
+            {...props}
+            to="/login"></Redirect>
+        }
+      }
+    ></Route>
+  )
+}
 
+function RedirectToFeed(props) {
+  let Component = props.comp;
+  // check -> are you loggedIN
+  let cUser = useContext(AuthContext);
+  // cuser-> null ->logi page
+  // cuser-> anything 
+  return (
+    <Route
+      {...props}
+      render={
+        (props) => {
+          // logic
+          return cUser != null ? <Redirect {...props} to="/feed">
+          </Redirect> : <Component
+            {...props}
+          ></Component>
+        }
+      }
+    ></Route>
+  )
+}
 export default App;
